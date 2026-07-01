bbb69800f4e7e162d75aa1776ec835bfc402f040
Sophie LDL <sophievigneron33@gmail.com>
2026-07-01 12:28:30 +0200

    (S1-US01) creating HTML of CocktailDetails component and fetch API in it

--- src/components/CocktailDetails/CocktailDetails.tsx
+++ src/components/CocktailDetails/CocktailDetails.tsx
@@ -1,7 +1,180 @@
+import { useEffect, useState } from "react";
+import { Cocktails, Ingredients } from "../../types/types";
 import styles from "./CocktailDetails.module.scss";
 
 function CocktailDetails() {
-	return <article className={styles["cocktail-card"]}>Details</article>;
+	const [cocktailDetails, setCocktailDetails] = useState("");
+	useEffect(() => {
+		const API_KEY = import.meta.env.VITE_API_KEY;
+		const BASE = `https://www.thecocktaildb.com/api/json/v2/${API_KEY}`;
+		fetch(`${BASE}/lookup.php?i=11007`)
+			.then((response) => response.json())
+			.then((data) => {
+				setCocktailDetails(data.drinks[0]);
+			});
+	}, []);
+	return (
+		<article className={styles["cocktail-card"]}>
+			{console.log(cocktailDetails)}
+			<h1>Details et recette de {cocktailDetails.strDrink}</h1>
+			<img
+				className={styles["cocktail-img"]}
+				src={cocktailDetails.strDrinkThumb}
+				alt="cocktail name"
+			/>
+			<img
+				className={styles.favheart}
+				src="../assets/icons/heart.svg"
+				alt="icon heart favorite"
+			/>
+			<h2>{cocktailDetails.strDrink}</h2>
+			<div className={styles["cocktail-tags"]}>
+				<ul>
+					<li>{cocktailDetails.strAlcoholic}</li>
+					<li>tag2</li>
+				</ul>
+			</div>
+			<div className="recipe-specs">
+				<p>10 min</p>
+				<p>2 pers.</p>
+				<p>Facile</p>
+			</div>
+			<h3>INGREDIENTS</h3>
+			<ul>
+				<li>
+					<img
+						className={styles["ingredient-img"]}
+						src={`https://www.thecocktaildb.com/images/ingredients/${cocktailDetails.strIngredient1}.png`}
+						alt=""
+					/>
+					{cocktailDetails.strIngredient1} {cocktailDetails.strMeasure1}
+				</li>
+				<li>
+					<img
+						className={styles["ingredient-img"]}
+						src={`https://www.thecocktaildb.com/images/ingredients/${cocktailDetails.strIngredient2}.png`}
+						alt=""
+					/>
+					{cocktailDetails.strIngredient2} {cocktailDetails.strMeasure2}
+				</li>
+				<li>
+					<img
+						className={styles["ingredient-img"]}
+						src={`https://www.thecocktaildb.com/images/ingredients/${cocktailDetails.strIngredient3}.png`}
+						alt=""
+					/>
+					{cocktailDetails.strIngredient3} {cocktailDetails.strMeasure3}
+				</li>
+				<li>
+					<img
+						className={styles["ingredient-img"]}
+						src={`https://www.thecocktaildb.com/images/ingredients/${cocktailDetails.strIngredient4}.png`}
+						alt=""
+					/>
+					{cocktailDetails.strIngredient4} {cocktailDetails.strMeasure4}
+				</li>
+				<li>
+					<img
+						className={styles["ingredient-img"]}
+						src={`https://www.thecocktaildb.com/images/ingredients/${cocktailDetails.strIngredient5}.png`}
+						alt=""
+					/>
+					{cocktailDetails.strIngredient5} {cocktailDetails.strMeasure5}
+				</li>
+				<li>
+					<img
+						className={styles["ingredient-img"]}
+						src={`https://www.thecocktaildb.com/images/ingredients/${cocktailDetails.strIngredient6}.png`}
+						alt=""
+					/>
+					{cocktailDetails.strIngredient6} {cocktailDetails.strMeasure6}
+				</li>
+				<li>
+					<img
+						className={styles["ingredient-img"]}
+						src={`https://www.thecocktaildb.com/images/ingredients/${cocktailDetails.strIngredient7}.png`}
+						alt=""
+					/>
+					{cocktailDetails.strIngredient7} {cocktailDetails.strMeasure7}
+				</li>
+				<li>
+					<img
+						className={styles["ingredient-img"]}
+						src={`https://www.thecocktaildb.com/images/ingredients/${cocktailDetails.strIngredient8}.png`}
+						alt=""
+					/>
+					{cocktailDetails.strIngredient8} {cocktailDetails.strMeasure8}
+				</li>
+				<li>
+					<img
+						className={styles["ingredient-img"]}
+						src={`https://www.thecocktaildb.com/images/ingredients/${cocktailDetails.strIngredient9}.png`}
+						alt=""
+					/>
+					{cocktailDetails.strIngredient9} {cocktailDetails.strMeasure9}
+				</li>
+				<li>
+					<img
+						className={styles["ingredient-img"]}
+						src={`https://www.thecocktaildb.com/images/ingredients/${cocktailDetails.strIngredient10}.png`}
+						alt=""
+					/>
+					{cocktailDetails.strIngredient10} {cocktailDetails.strMeasure10}
+				</li>
+				<li>
+					<img
+						className={styles["ingredient-img"]}
+						src={`https://www.thecocktaildb.com/images/ingredients/${cocktailDetails.strIngredient11}.png`}
+						alt=""
+					/>
+					{cocktailDetails.strIngredient11} {cocktailDetails.strMeasure11}
+				</li>
+				<li>
+					<img
+						className={styles["ingredient-img"]}
+						src={`https://www.thecocktaildb.com/images/ingredients/${cocktailDetails.strIngredient12}.png`}
+						alt=""
+					/>
+					{cocktailDetails.strIngredient12} {cocktailDetails.strMeasure12}
+				</li>
+				<li>
+					<img
+						className={styles["ingredient-img"]}
+						src={`https://www.thecocktaildb.com/images/ingredients/${cocktailDetails.strIngredient13}.png`}
+						alt=""
+					/>
+					{cocktailDetails.strIngredient13} {cocktailDetails.strMeasure13}
+				</li>
+				<li>
+					<img
+						className={styles["ingredient-img"]}
+						src={`https://www.thecocktaildb.com/images/ingredients/${cocktailDetails.strIngredient14}.png`}
+						alt=""
+					/>
+					{cocktailDetails.strIngredient14} {cocktailDetails.strMeasure14}
+				</li>
+				<li>
+					<img
+						className={styles["ingredient-img"]}
+						src={`https://www.thecocktaildb.com/images/ingredients/${cocktailDetails.strIngredient15}.png`}
+						alt=""
+					/>
+					{cocktailDetails.strIngredient15} {cocktailDetails.strMeasure15}
+				</li>
+			</ul>
+			<h3>PRÉPARATION</h3>
+			<p>{cocktailDetails.strInstructionsFR}</p>
+			<h3>INFORMATIONS</h3>
+			<ul>
+				<li>Type de Verre : {cocktailDetails.strGlass}</li>
+				<li>
+					<abbr title="International Bartenders Association : ">IBA</abbr> :{" "}
+					{cocktailDetails.strIBA}
+				</li>
+				<li>Information3</li>
+			</ul>
+		</article>
+	);
 }
 
 export default CocktailDetails;
