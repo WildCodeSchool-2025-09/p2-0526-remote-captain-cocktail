493cb589c95453b606859c59d16051d0a845dec9
Sophie LDL <sophievigneron33@gmail.com>
2026-07-01 09:57:50 +0200

    (S1-US01) created CocktailDetails component and CocktailDetails module sass

---
+++ src/components/CocktailDetails/CocktailDetails.tsx
@@ -0,0 +1,7 @@
+import styles from "./CocktailDetails.module.scss";
+
+function CocktailDetails() {
+	return <article className={styles["cocktail-card"]}>Details</article>;
+}
+
+export default CocktailDetails;
