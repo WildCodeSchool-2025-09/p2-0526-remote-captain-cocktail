23d955a825298a88401fd7dc05963235bc33bb1c
Frederic Briand <fbr91144@gmail.com>
2026-07-01 10:34:47 +0200

    how to map

--- src/pages/Bar/Bar.tsx
+++ src/pages/Bar/Bar.tsx
@@ -3,13 +3,33 @@ import IngredientsSearch from "../../components/IngredientsSearch/IngredientsDro
 import MyIngredients from "../../components/MyIngredients/MyIngredients";
 import MySuggestions from "../../components/MySuggestions/MySuggestions";
 
+import styles from "./Bar.module.scss";
+
+import t from "../../data/fr_FR.json";
+
 function Bar() {
 	return (
-		<div>
+		<div className={styles["bar-page"]}>
 			<IngredientsSearch />
-			<IngredientsDropdown />
-			<MyIngredients />
-			<MySuggestions />
+			<div className="how-to">
+				<h3>{t.bar.howTo.title}</h3>
+				<ul>
+					{t.bar.howTo.steps.map((step, i) => (
+						<li key={step.id}>
+							<span>{i + 1}</span>
+							<div>
+								<span>{step.line1}</span>
+								<span>{step.line2}</span>
+							</div>
+						</li>
+					))}
+				</ul>
+			</div>
+			<div>
+				<IngredientsDropdown />
+				<MyIngredients />
+				<MySuggestions />
+			</div>
 		</div>
 	);
 }
