b70b085dbd51e2ba12d940b7cb91d76fe8141589
Sophie LDL <sophievigneron33@gmail.com>
2026-07-06 14:24:51 +0200

    (S2-US04) Remove CocktailDetails component from Favorites, used for tests

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,10 +1,7 @@
-import CocktailDetails from "../../components/CocktailDetails/CocktailDetails";
-
 function Favorites() {
 	return (
 		<>
 			<h1>Hello from Favorites</h1>
-			<CocktailDetails />
 		</>
 	);
 }
