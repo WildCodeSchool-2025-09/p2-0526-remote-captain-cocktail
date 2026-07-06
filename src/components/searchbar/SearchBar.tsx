556ce2871f4d344d0a980c5dcbf2041111e53e68
THOMAS FABULET <thomas.fabulet@gmail.com>
2026-07-06 23:29:55 +0200

    add a sort alphabetical button in searchbar

--- src/components/searchbar/SearchBar.tsx
+++ src/components/searchbar/SearchBar.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import type { Category } from "../../types/types";
 import styles from "./SearchBar.module.scss";
+import sortIcon from "../../assets/icons/sort.svg";
 
 interface SearchBarProps {
 	searchQuery: string;
@@ -9,11 +10,20 @@ interface SearchBarProps {
 	selectedCategory: string;
 	alcoholicFilter: string;
 	setAlcoholicFilter: (filter: string) => void;
+	onSortChange?: (isAscending: boolean) => void,
 }
 
 function SearchBar({
-	searchQuery, setSearchQuery, selectedCategory, setSelectedCategory, alcoholicFilter, setAlcoholicFilter }: SearchBarProps) {
+	searchQuery,
+	setSearchQuery,
+	selectedCategory,
+	setSelectedCategory,
+	alcoholicFilter,
+	setAlcoholicFilter,
+	onSortChange,	
+}: SearchBarProps) {
 	const [categories, setCategories] = useState<Category[]>([]);
+	const [sortAscending, setSortAscending] = useState<boolean>(true);
 
 	const API_KEY = import.meta.env.VITE_API_KEY;
 	const BASE = `https://www.thecocktaildb.com/api/json/v2/${API_KEY}`;
@@ -33,39 +43,52 @@ function SearchBar({
 
 	return (
 		<>
-			<div className={styles.searchBarContainer}>
-				<div className={styles.inputwrapper}>
+			<div className={styles["searchbar-container"]}>
+				<div className={styles["input-wrapper"]}>
 					<input
-						className={styles.searchInput}
+						className={styles["search-input"]}
 						type="text"
-						placeholder="Search for a cocktail or an ingredient"
+						placeholder="Search..."
 						value={searchQuery}
 						onChange={(e) => setSearchQuery(e.target.value)}
 					/>
 					{searchQuery && (
 						<button
-							className={styles.clearButton}
+							type="button"
+							className={styles["clear-button"]}
 							onClick={() => setSearchQuery("")}
 						>
 							×
 						</button>
 					)}
 				</div>
+				<button
+					type="button"
+    				className={`${styles["sort-button"]} ${sortAscending ? styles["active-sort-asc"] : styles["active-sort-desc"]}`}
+    				onClick={() => {
+        				setSortAscending(!sortAscending);
+        				onSortChange?.(!sortAscending);
+    				}}
+    				title={sortAscending ? "Trier Z→A" : "Trier A→Z"}
+				>
+    			<img src={sortIcon} alt="Sort" className={styles["sort-icon"]} />
+				</button>
+				
 			</div>
-			<div className={styles.filterContainer}>
-				<div className={styles.badgeContainer}>
+			<div className={styles["filter-container"]}>
+				<div className={styles["badge-container"]}>
 					{[
 						{ label: "All", value: "" },
 						{ label: "With alcohol", value: "Alcoholic" },
 						{ label: "Without alcohol", value: "Non alcoholic" },
-						{ label: "Optionnal", value: "Optional alcohol" }
-
+						{ label: "Optionnal", value: "Optional alcohol" },
 					].map((type) => {
 						const isSelected = alcoholicFilter === type.value;
-						const badgeClass = `${styles.badge} ${isSelected ? styles.activeAlcool : ""}`;
+						const badgeClass = `${styles.badge} ${isSelected ? styles["active-alcool"] : ""}`;
 
 						return (
 							<button
+								type="button"
 								key={type.value}
 								onClick={() => setAlcoholicFilter(type.value)}
 								className={badgeClass}
@@ -76,16 +99,19 @@ function SearchBar({
 					})}
 				</div>
 			</div>
-			<div className={styles.filterContainer}>
-				<div className={styles.badgeContainer}>
+			<div className={styles["filter-container"]}>
+				<div className={styles["badge-container"]}>
 					{categories.map((category) => {
 						const isSelected = selectedCategory === category.strCategory;
-						const badgeClass = `${styles.badge} ${isSelected ? styles.activeCategory : ""}`;
+						const badgeClass = `${styles.badge} ${isSelected ? styles["active-category"] : ""}`;
 
 						return (
 							<button
+								type="button"
 								key={category.strCategory}
-								onClick={() => setSelectedCategory(isSelected ? "" : category.strCategory)}
+								onClick={() =>
+									setSelectedCategory(isSelected ? "" : category.strCategory)
+								}
 								className={badgeClass}
 							>
 								{category.strCategory}
@@ -97,5 +123,5 @@ function SearchBar({
 		</>
 	);
 }
-export default SearchBar;
 
+export default SearchBar;
