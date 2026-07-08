20050a1d056e9c8d9b9f5f0dd99d16b2a93cad08
THOMAS FABULET <thomas.fabulet@gmail.com>
2026-07-08 17:00:35 +0200

    edit foldername SearchBar

--- src/components/searchbar/SearchBar.tsx
+++ src/components/searchbar/SearchBar.tsx
@@ -99,6 +99,7 @@ function SearchBar({
 				</div>
 			</div>
 			<div className={styles["filter-container"]}>
+				
 				<div className={styles["badge-container"]}>
 					{categories.map((category) => {
 						const isSelected = selectedCategory === category.strCategory;
