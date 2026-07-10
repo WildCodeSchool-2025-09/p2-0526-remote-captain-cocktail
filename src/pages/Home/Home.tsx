05f23c0c5c4a623d2e294e8f228725c61120dfb9
Sophie LDL <sophievigneron33@gmail.com>
2026-07-10 14:17:25 +0200

    (S2-US06) Added content to Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,49 @@
+import { Link } from "react-router-dom";
+import Icon from "../../components/Icon/Icon";
+import styles from "./Home.module.scss";
+
 function Home() {
 	return (
-		<>
-			<h1>Hello from Home</h1>
-		</>
+		<section className={styles.home}>
+			<img
+				src="../../assets/images/logotransp.png"
+				alt="Captain Cocktail's logo"
+				className={styles.logo}
+			/>
+			<p>
+				A cocktail catalog right at your fingertips, tailored to what you
+				already have at home.
+			</p>
+			<article>
+				<Icon name="readytoshake" />
+				<h2>Prêt à shaker ?</h2>
+				<p>
+					Add your ingredients and find your next favorite cocktail in seconds.
+				</p>
+				<button type="button" className="pink-button">
+					Add Ingredients
+				</button>
+				<p>It's fast, free, and requires no registration.</p>
+			</article>
+			<article>
+				<h2>Cocktail of the moment</h2>
+				<div>test</div>
+			</article>
+			<article>
+				<h3>All cocktails</h3>
+				<p>test</p>
+				<button type="button" className="pink-button">
+					<Link to="/cocktails">Browse</Link>
+				</button>
+			</article>
+			<article> nbRealisable</article>
+			<article>Realisables maintenant</article>
+			<article>suggestions = un cocktail random parmi les réalisables</article>
+			<article>
+				<h2>Recents favorites</h2>
+				<Link to="/favorites">See all</Link>
+			</article>
+		</section>
 	);
 }
 
