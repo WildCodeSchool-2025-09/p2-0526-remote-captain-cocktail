ca2fff4ff98f6e90df742f10327830ebf13b3337
Cynth <cynthiaflores64990@gmail.com>
2026-07-15 15:40:28 +0200

    (S2-US10) adjust CocktailTrending design and display it in Home

--- src/components/CocktailTrending/CocktailTrending.tsx
+++ src/components/CocktailTrending/CocktailTrending.tsx
@@ -5,29 +5,32 @@ import styles from "./CocktailTrending.module.scss";
 function CocktailTrending() {
 	const { setCocktailId } = useCocktailId();
 	return (
-		<section
-			className={styles["cocktail-trending"]}
-			aria-label={blueSignature.strDrink}
-		>
-			<div className={styles["cocktail-info"]}>
-				<h2 className={styles["cocktail-name"]}>{blueSignature.strDrink}</h2>
-				<p className={styles["cocktail-tagline"]}>World Cup Edition</p>
-				<button
-					type="button"
-					className={styles["btn-recipe"]}
-					command="show-modal"
-					commandfor="my-dialog"
-					onClick={() => setCocktailId(blueSignature.idDrink)}
-				>
-					See recipe
-				</button>
-			</div>
-			<img
-				className={styles["cocktail-img"]}
-				src="/assets/images/cocktails/blue-signature-cut.png"
-				alt=""
-			/>
-		</section>
+		<>
+			<h2>Trending Cocktail</h2>
+			<section
+				className={styles["cocktail-trending"]}
+				aria-label={blueSignature.strDrink}
+			>
+				<div className={styles["cocktail-info"]}>
+					<h3 className={styles["cocktail-name"]}>{blueSignature.strDrink}</h3>
+					<p className={styles["cocktail-tagline"]}>World Cup Edition</p>
+					<button
+						type="button"
+						className={`${styles["btn-recipe"]} pink-button`}
+						command="show-modal"
+						commandfor="my-dialog"
+						onClick={() => setCocktailId(blueSignature.idDrink)}
+					>
+						See recipe
+					</button>
+				</div>
+				<img
+					className={styles["cocktail-img"]}
+					src="/assets/images/cocktails/blue-signature-cut.png"
+					alt=""
+				/>
+			</section>
+		</>
 	);
 }
 
