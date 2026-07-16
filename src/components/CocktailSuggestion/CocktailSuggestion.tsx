8bfe7ce0099e13373f50a10e6e5647ee107060f3
THOMAS FABULET <thomas.fabulet@gmail.com>
2026-07-16 11:01:19 +0200

    add favorites button to CocktailSuggestion

--- src/components/CocktailSuggestion/CocktailSuggestion.tsx
+++ src/components/CocktailSuggestion/CocktailSuggestion.tsx
@@ -1,4 +1,5 @@
 import { useCocktailId } from "../../contexts/CocktailDetailsContext";
+import { useFavorites } from "../../contexts/FavoritesContext";
 import type { Cocktail } from "../../types/types";
 
 import styles from "./CocktailSuggestion.module.scss";
@@ -13,6 +14,9 @@ export default function CocktailSuggestion({
 	missingCount,
 }: { suggestion: Cocktail; missingCount: number }) {
 	const { setCocktailId } = useCocktailId();
+	const { isFavorite, toggleFavorite } = useFavorites();
+	const isCurrentFavorite = isFavorite(suggestion.idDrink);
+
 	return (
 		<button
 			type="button"
@@ -44,6 +48,19 @@ export default function CocktailSuggestion({
 					)}
 				</span>
 			</div>
+			<button
+				type="button"
+				className={styles["btn-favorite"]}
+				aria-label={
+					isCurrentFavorite ? "Remove from favorites" : "Add to favorites"
+				}
+				onClick={() => toggleFavorite(suggestion.idDrink)}
+			>
+				<Icon
+					name={isCurrentFavorite ? "fullheart" : "heart"}
+					className={styles["icon-heart"]}
+				/>
+			</button>
 		</button>
 	);
 }
